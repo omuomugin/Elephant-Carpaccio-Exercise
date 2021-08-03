0fdd94d376f80e51daa7873003414c4efd3b4f6c
omuomugin <omuomugin@out-box.dev>
2021-08-04 00:41:45 +0900

    15 story sliced

--- src/main/kotlin/DiscountCalculationRunner.kt
+++ src/main/kotlin/DiscountCalculationRunner.kt
@@ -5,10 +5,12 @@ class DiscountCalculationRunner {
     companion object {
         private const val DISCOUNT_RATE_OVER_1000 = 0.97
         private const val DISCOUNT_RATE_OVER_5000 = 0.95
+        private const val DISCOUNT_RATE_OVER_7000 = 0.93
     }
 
     fun run(totalPrice: Double): Double {
         val discountRate = when {
+            totalPrice >= 7000 -> DISCOUNT_RATE_OVER_7000
             totalPrice >= 5000 -> DISCOUNT_RATE_OVER_5000
             totalPrice >= 1000 -> DISCOUNT_RATE_OVER_1000
             else -> {
--- src/test/kotlin/DiscountCalculationRunnerTest.kt
+++ src/test/kotlin/DiscountCalculationRunnerTest.kt
@@ -47,4 +47,19 @@ class DiscountCalculationRunnerTest {
         // Assert
         Assert.assertThat(actual, CoreMatchers.`is`(expected))
     }
+
+    @Test
+    fun testRun_over_7000() {
+        // Arrange
+        val totalPrice = 7000.0
+
+        // Action
+        val actual = DiscountCalculationRunner().run(totalPrice)
+
+        // Expect
+        val expected = 6510.0
+
+        // Assert
+        Assert.assertThat(actual, CoreMatchers.`is`(expected))
+    }
 }
